06583c44c728049009f07618813951bf1c77af9e
aniken998 <enzhou@ucsc.edu>
2020-02-04 21:34:33 -0800

    push again

--- P4/run.py
+++ P4/run.py
@@ -54,16 +54,12 @@ def test(bot, opponent_bot, map_num):
 
 if __name__ == '__main__':
     path =  os.getcwd()
-<<<<<<< HEAD
     # opponents = ['opponent_bots/easy_bot.py']
     opponents = ['opponent_bots/spread_bot.py']
     # opponents = ['opponent_bots/aggressive_bot.py']
     
     '''
     ,
-=======
-    opponents = ['opponent_bots/easy_bot.py',
->>>>>>> 5ac0a059bbc68863ef8fd4d057b040e8eaad8de8
                  'opponent_bots/spread_bot.py',
                  'opponent_bots/aggressive_bot.py',
                  'opponent_bots/defensive_bot.py',
